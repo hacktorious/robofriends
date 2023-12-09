91e132d4986575e97385f3ece3ec1c715f10ad72
Scott Macri <2849321+hacktorious@users.noreply.github.com>
2023-12-09 14:51:00 -0500

    first commit

---
+++ src/components/ErrorBoundry.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+class ErrorBountry extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oops, that is not good!</h1>
+        }
+        return this.props.children
+    }
+}
+
+export default ErrorBountry
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+const SearchBox = ({searchField, searchChange}) => {
+    return (
+        <div className="tc">
+            <input 
+                className='pa3 ba b--green bg-lightest-blue'
+                type='search'
+                placeholder='search robots' 
+                onChange={searchChange} 
+            />
+        </div>
+    )
+}
+
+export default SearchBox
